51e58b2e1d57cf75bf7019c8c8e17935bbe9acec
Darshan Patel <201851081@iiitvadodara.ac.in>
2022-06-01 18:32:20 +0530

    port added to server

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ var cors = require('cors');
 const { data } = require("./data");
 
 const app = express();
+
+const PORT = process.env.PORT || 8080;
+
 app.use(cors());
 
 app.get("/api/data", (req, res, next) => {
     res.json(data);
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
  console.log("Server running on port 8080");
 });
